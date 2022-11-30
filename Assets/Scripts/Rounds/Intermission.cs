2e919cea487ecdfbf94799303fa5fc5b9629dcf4
Jordan <theghastlytuna@gmail.com>
2022-11-30 12:52:06 -0500

    Removed phantom poker tables

--- Assets/Scripts/Rounds/Intermission.cs
+++ Assets/Scripts/Rounds/Intermission.cs
@@ -9,7 +9,7 @@ public class Intermission : Round
     public Intermission()
     {
         type = roundType.INTERMISSION;
-        roundTime = 10;
+        roundTime = 60;
         hasMap = true;
     }
 

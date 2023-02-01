dd9a9fe90046875a8a407e8cc69c638c0274bcf8
Ryan <ryan.dinh@hotmail.com>
2023-01-31 19:29:09 -0500

    New Networking and client side start
    Folder created, and client side script added

---
+++ Assets/Scripts/Networking/ClientNetwork.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientNetwork : MonoBehaviour
+{
+	public static void StartClient()
+	{
+		byte[] buffer = new byte[256];
+	}
+
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		StartClient();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

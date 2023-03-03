3ed0d6ff38c250b4197fd3d47befd39b1c858dd9
Gohembo <gohembo@gmail.com>
2023-03-03 18:54:06 -0500

    networking additions
    ignore playground V4 updates. TODO: replace pazzis code in the server with our own, find a asyncronous way to do TCP, and implement UDP

--- Assets/Scripts/Managers/NetworkManager.cs
+++ Assets/Scripts/Managers/NetworkManager.cs
@@ -4,154 +4,357 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using UnityEditor;
 using UnityEngine;
 
 public class NetworkManager : MonoBehaviour
 {
-    public static NetworkManager instance;
+    //misc
+    public List<string> InstructionTypeCodes = new List<string>{"@","!","#","$","+"};
     public bool isServer = false;
-    public static string hostIP;
-    public static int port = 8888;
     public float secondsBetweenUpdates = 0.5f;
-    float secondsElapsed = 0f;
+    public int TCPPort = 8888;
+    public int UDPPort = 8889;
+    public string serverIP;
+    public float timeoutSeconds = 2f;
+    int playersConnected = 0;
 
-    //client
-    private static byte[] outBuffer = new byte[512];
-    private static IPEndPoint serverEndPoint;
-    private static Socket client;//client socket to server (client > server)
-    private List<NetData> miscDataToSend = new List<NetData>();//game state changes, dodgeball throws stuff like that
-    private NetData playerTransform = new NetData();//player movement and rotation
+    public static NetworkManager instance;
+
+  
 
 
+    //network and socket stuff
+    private static byte[] buffer = new byte[512];
+    private static IPHostEntry hostInfo;
+    private static IPAddress ip;
+    private static IPEndPoint endPointTCP;//the IP and port of the recipient for TCP
+    private static IPEndPoint endPointUDP;//the IP and port of the recipient for UDP
+    private static Socket TCPSocket;
+    private static Socket UDPSocket;
+    Dictionary<NetworkScript, List<NetworkInstruction>> queuedInstructions = new Dictionary<NetworkScript, List<NetworkInstruction>>();
 
-    // Start is called before the first frame update
-    void Start()
+
+    //client only
+    public static EndPoint receiver;
+
+    //server only (now a console app)
+    //List<NetworkedClient> clients = new List<NetworkedClient>();
+    //List<List<int>> gameobjectIDs = new List<List<int>>();//for server only
+
+    //booleans for async tasks
+    bool hasSetupEndpoint = false;
+    bool setupTCP = false;
+    bool setupUDP = false;
+
+    //receive functions for TCP and UDP
+    private void setupUDPClient()
     {
-        return;
+        if (isServer)
+        {
+            return;
+        }
+        
+        Debug.Log("Setup UDP Client");
+        setupUDP = true;
+
+    }
+
+    private async void setupTCPClient()//https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/socket-services
+    {
+        if (isServer)
+        {
+            return;
+        }
+        
+        TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+       
+        await TCPSocket.ConnectAsync(endPointTCP);
+        Debug.Log("Setup TCP Client");
+        setupTCP = true;
+        
+    } 
+
+    private void setupTCPServer()
+    {
+        if(endPointTCP == null)
+        {
+            Debug.Log("ENDPOINT NULL");
+        }
+        
+        TCPSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream,ProtocolType.Tcp);
+
+        try
+        {
+            TCPSocket.Bind(endPointTCP);
+            TCPSocket.Listen(4);//# of players, pretty much
+            //TCPSocket.Accept(); //adding this in makes the editor always get stuck on EditorPlayMode or some crap, never add this line in here
+        }
+        catch (Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+        setupTCP = true;
+        Debug.Log("Setup TCP Server");
+    }
+    private void setupUDPServer()
+    {
+        setupUDP = true;
+        Debug.Log("Setup UDP Server");
+    }
+
+    private void setupEndPoint()
+    {
+        //https://learn.microsoft.com/en-us/dotnet/fundamentals/networking/sockets/socket-services
+        try
+        {
+
+
+            //hostInfo = await Dns.GetHostEntryAsync(serverIP);
+            ip = IPAddress.Parse(serverIP);//hostInfo.AddressList[0];
+            if (isServer)
+            {
+                ip = IPAddress.Any;//so we listen for any incoming connections
+            }
+            endPointUDP = new IPEndPoint(ip, UDPPort);
+
+            endPointTCP = new IPEndPoint(ip, TCPPort);
+
+            hasSetupEndpoint = true;
+           
+            Debug.Log("Setup endpoint");
+        }
+        catch (Exception e)
+        {
+           
+            if(e is SocketException)
+            {
+                Debug.LogError("Socket error, check IP");
+            }
+            else
+            {
+                Debug.LogError(e.Message);
+            }
+        }
+        
+        
+
+        
+    }
+
+    public string getIPAddress()
+    {
+        //https://stackoverflow.com/questions/6803073/get-local-ip-address
+        var host = Dns.GetHostEntry(Dns.GetHostName());
+        foreach (var ip in host.AddressList)
+        {
+            if (ip.AddressFamily == AddressFamily.InterNetwork)
+            {
+                return ip.ToString();
+            }
+        }
+        throw new Exception("No network adapters with an IPv4 address in the system!");
+    }
+
+    public string getPlayerName()
+    {
+        //go onto AWS and get the name or something, maybe make it local??
+        return "Jhon Gaming";
+    }
+
+    public string getGameObjectCode(GameObject o)
+    {
+        return o.GetInstanceID().ToString();
+    }
+
+    public void waitForNewClient()
+    {
+        //Socket client = TCPSocket.Accept();//will stop here
+        TCPSocket.Accept();
+        playersConnected++;
+        //registerClient(client.)
+    }
+
+    public void registerClient(IPAddress address, Socket s, string name)
+    {
+       
+        
+          
+        
+        
+    }
+
+    public void registerGameObject(GameObject g)
+    {
+        //send request to server
+    }
+
+    public void createAndRegisterGameObject(string prefab, Transform objectTransform)
+    {
+        //create gameobject in world
+        GameObject newObject = Resources.Load<GameObject>(prefab);
+        registerGameObject(newObject);
+       
+
+
+
+
+
+    }
+
+    public void queueInstruction(NetworkScript script, NetworkInstruction instruction)
+    {
+        if (!queuedInstructions.ContainsKey(script))
+        {
+            queuedInstructions.Add(script, new List<NetworkInstruction>());
+        }
+
+        foreach(NetworkInstruction i in queuedInstructions[script])//go through all instructions queued by the script
+        {
+            if(i.code == instruction.code)//check if an instruction with the same code has been issued
+            {
+                i.codeData = instruction.codeData;//if so, replace it
+                return;
+            }
+        }
+        queuedInstructions[script].Add(instruction);//at this point, there is a list of instructions made, and the type of instruction is unique, so add it in to the list
+    }
+
+
+    private void Awake()
+    {
         if(instance == null)
         {
             instance = this;
         }
+       // gameobjectIDs.Add(new List<int>());//reserve list of gameobjects at index
+        setupNetworking();
+        
+    }
+
+    private void Update()
+    {
+       
+        if (!setupTCP)
+        {
+            return;
+        }
+        if(playersConnected == 0)
+        {
+            return;
+        }
+        int recv = TCPSocket.Receive(buffer);
+        if (recv > 0)
+        {
+            Debug.Log(Encoding.ASCII.GetString(buffer, 0, recv));
+
+            //byte[] sending = new byte[512];
+            recv = 0;
+
+        }
+    }
+
+    public void setupNetworking()
+    {
+        setupEndPoint();
         if (isServer)
         {
-            startServer();
-        }
-        StartClient();
-    }
-
-
-    public static void StartClient()
-    {
-        try
-        {
-            IPAddress ip = IPAddress.Parse(hostIP);
-
-            serverEndPoint = new IPEndPoint(ip, port);
-
-            client = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);
-
-
-
-        }
-        catch (Exception e)
-        {
-            Debug.Log(e.Message);
-        }
-    }
-
-    public static void startServer()
-    {
-        //just receiving data, buffer for sending
-        byte[] buffer = new byte[512];
-        //getting ip from machine automatically
-        IPHostEntry hostInfo = Dns.GetHostEntry(Dns.GetHostName());
-
-        IPAddress ip = hostInfo.AddressList[01];//all network interfaces on your machine
-        //0 is v6, 1 is v4
-
-        //Console.WriteLine("HOST: {0} , IP {1}", hostInfo.HostName, ip.ToString());
-
-        IPEndPoint localEndPoint = new IPEndPoint(ip, port);
-
-        Socket server = new Socket(ip.AddressFamily, SocketType.Dgram, ProtocolType.Udp);//this is our server
-
-        EndPoint client = new IPEndPoint(IPAddress.Any, 0);//can be from any ip address in the world. DNS sets the port
-
-        //we dont have to listen for anything
-
-        try
-        {
-            server.Bind(localEndPoint);
-
-            Console.WriteLine("waiting for data");
-
-            //lets always receive data
-
-            while (true)
-            {
-                int receivedBytesCount = server.ReceiveFrom(buffer, ref client);
-                //server.sendto 
-                if (receivedBytesCount == 0)
-                {
-                    continue;
-                }
-               // Console.WriteLine("Received from: {0} -> {1}", client.ToString(), Encoding.ASCII.GetString(buffer, 0, receivedBytesCount));
-            }
-
-           
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine("ERROR: {0}", e.Message);
+            setupTCPServer();
+            setupUDPServer();
+            return;
         }
 
-    }
 
-   
-
-    // Update is called once per frame
-    void Update()
-    {
-        return;
-        secondsElapsed += Time.deltaTime;
-        if(secondsElapsed >= secondsBetweenUpdates)
-        {
-            //send data to server
-            playerTransform = new NetData(GameManager.instance.clientPlayer.transform);
-
-            outBuffer = new byte[512];
-            outBuffer = Encoding.ASCII.GetBytes(playerTransform.getDataString());
-            client.SendTo(outBuffer, serverEndPoint);
-
-            foreach (NetData data in miscDataToSend)
-            {
-                outBuffer = new byte[512];
-                outBuffer = Encoding.ASCII.GetBytes(data.getDataString());
-                client.SendTo(outBuffer, serverEndPoint);
-                
-            }
-        }
+        setupTCPClient();
+        setupUDPClient();
     }
 }
 
-public class NetData
+public class NetworkInstruction
 {
-    string sendingData = "";
+    public string localGameObject;//the gameobject in our NVE that we want to affect
+    public string localIPAddress;//for indexing the gameobject later
+    public InstructionType code;
+    public object codeData; //(converted to a string later through JsonUtility)
 
-    public NetData() { }
-
-
-    public NetData(Transform t)
+    public NetworkInstruction(GameObject affected, InstructionType instruction, object data )
     {
-        sendingData += t.position.ToString() + "\n";
-        sendingData += t.rotation.eulerAngles.ToString() + "\n";
-        sendingData += t.localScale.ToString() + "\n";
+        localGameObject = NetworkManager.instance.getGameObjectCode(affected);
+        localIPAddress = NetworkManager.instance.getIPAddress();
+        code = instruction;
+        codeData = data;
     }
 
-    public string getDataString()
+    public string getString()
     {
-        return sendingData;
+        return NetworkManager.instance.InstructionTypeCodes[(int)code] + JsonUtility.ToJson(codeData);
+    }
+   
+}
+
+public enum InstructionType
+{
+    LOCAL_GAMEOBJECT_ID,
+    POWERUP_USE,
+    TRANSFORM_CHANGE,
+    VELOCITY_CHANGE,
+    REGISTER_GAMEOBJECT
+}
+/*
+ * CODES FOR NETWORKED STRING
+ * @ (int) - gameobject ID to modify
+ * ! (powerupType) - powerup used (giving powerups can be local) (called in onUse for powerups)
+ * # (Transform) - set transform change
+ * $ (Vec3) - velocity change (for dead reckoning)
+ * 
+ * | - used to separate instructions
+ * 
+ * to send data, call the function XXXX which will check if there is already an instruction to modify the same type. if there is, then it will be replaced.
+ */
+
+public class NetworkedClient {
+    IPAddress address;
+    string name;
+    Socket clientSocket;
+
+    public NetworkedClient(IPAddress ip, Socket socket, string playerName)
+    {
+        name = playerName;
+        address = ip;
+        clientSocket = socket;
+    }
+
+    public Socket getSocket()
+    {
+        return clientSocket;
+    }
+
+    public IPAddress getAddress()
+    {
+        return address;
+
+    }
+
+    public string getPlayerName()
+    {
+        return name;
     }
 }
+
+[CustomEditor(typeof(NetworkManager))]
+public class NetworkManagerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+       
+        if (GUILayout.Button("Wait for client"))
+        {
+            NetworkManager.instance.waitForNewClient();
+        }
+    }
+}
+
 
 
